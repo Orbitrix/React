efa2729b968a58a234b5468cc62ae847b595b1e9
Mohd Zaid <mohdzaid4659@gmail.com>
2025-06-16 00:11:09 +0530

    feat: initialize React project with Vite and ESLint
    - Added package.json for project dependencies and scripts.
    - Included Vite logo SVG in the public directory.
    - Created App.css for styling the application.
    - Developed App.jsx component with counter functionality.
    - Added React logo SVG in assets.
    - Established global styles in index.css.
    - Set up main entry point in main.jsx.
    - Configured Vite with react plugin in vite.config.js.

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,12 +1,14 @@
 import Orbitrix from "./orbitrix"
 
 function App() {
+  const username = "TinkPad ТинкПад"
 
   return (
     <>
-    <h1>Orbiteix aur react with vite | Мохд Заид</h1>
-    
-    <Orbitrix/>
+      <h1>Orbiteix aur react with vite | Мохд Заид {username}</h1>
+      <h1>Орбитеих аур реацт житх вите | Мохд Заид {username}</h1>
+
+      <Orbitrix />
     </>
   )
 }
--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -1,8 +1,37 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App | Orbitrix</h1>
+    </div>
+  )
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//     href: 'https://google.com',
+//     target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+// const reactElements = React.createElement(
+//   'a',
+//   {href: 'https://google.com',target: '_blanck'},
+//   'click me to visit google'
+
+// )
+
+
+
 createRoot(document.getElementById('root')).render(
+
+  
+  MyApp,
   <StrictMode>
     <App />
   </StrictMode>,
