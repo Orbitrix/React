5ba373c8fd57b246f1fa8ef38d76803625ab9684
Mohd Zaid <mohdzaid4659@gmail.com>
2025-10-17 22:06:47 +0530

    create a pages

--- 12_MegaBlog/src/components/index.js
+++ 12_MegaBlog/src/components/index.js
@@ -5,7 +5,12 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button"
 import Input from "./Input"
+import PostForm from "./post-form/PostForm"
 import RTE from "./RTE";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import PostCard from "./PostCard";
+import EditPost from "../pages/EditPost";
 
 export{
     Header,
@@ -15,5 +20,10 @@ export{
     LogoutBtn,
     Button,
     Input,
-    RTE
+    PostForm,
+    RTE,
+    Signup,
+    Login,
+    PostCard,
+    EditPost
 }
---
+++ 12_MegaBlog/src/pages/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Signup as SingupComponents } from '../components'
+
+function Signup() {
+  return (
+    <div className='py-8'>
+        <SingupComponents />
+    </div>
+  )
+}
+
+export default Signup
